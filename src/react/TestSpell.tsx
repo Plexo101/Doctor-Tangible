3f96f15b314fdfaacfbae8ffabe670f8e0aa7a1d
LunaeSomnia <adrianmoralestorrano@gmail.com>
2021-03-19 12:27:13 +0100

    Update TestSpell.tsx
    Now it functions properly.
    Known issues:
    - Not all properties are used
    - We could move all the interfaces into its own file if needed

--- src/react/TestSpell.tsx
+++ src/react/TestSpell.tsx
@@ -3,17 +3,112 @@ import { render } from 'react-dom'
 import * as data from '../../data/spells/spells-xge.json'
 import * as stringers from '../other/stringers'
 
+interface Serializable<T> {
+    deserialize(input: Object): T;
+}
+
 interface Props {
     index: number;
     max: number;
 }
 
-interface Time {
-    number: number;
-    unit: string;
+interface SpellSource {
+    source: string,
+    page: number
+}
+
+interface SpellTime{
+    number: number,
+    unit: string,
+    condition: string
+}
+
+interface SpellRange {
+    type: string,
+    distance : SpellRangeDistance
+}
+
+interface SpellRangeDistance {
+    type: string,
+    amount: number
+}
+
+interface SpellComponents {
+    v: boolean,
+    s: boolean,
+    m: SpellComponentsMaterials | string
+}
+
+interface SpellComponentsMaterials {
+    text: string,
+    cost: number,
+    consume: boolean
+}
+
+interface SpellDuration {
+    type: string,
+    duration: SpellDurationDuration
+    ends: string[],
+    concentration: boolean
+}
+
+interface SpellDurationDuration {
+    type: string,
+    amount: number
+}
+
+interface SpellEntries {
+    type: string,
+    name: string,
+    entries: string[]
+}
+
+interface SpellScalingLevelDice {  //Only for cantrips
+    lavel: string,
+    scaling: SpellScalingLevelDiceScaling
+}
+
+interface SpellScalingLevelDiceScaling {
+    1: string,
+    5: string,
+    11: string,
+    17: string
+}
+
+interface SpellClassesGroup {
+    fromClassList: SpellClasses[],
+    fromClassListVariant: SpellClasses[],
+    backgrounds: SpellClasses[]
+}
+
+interface SpellClasses {
+    name: string,
+    source: string,
+    definedInSource: string
+}
+
+interface Spell {
+    name: string;
+    source: SpellSource;
+    otherSources: SpellSource[];
+    level: number;
+    school: string;
+    time: SpellTime[];
+    range: SpellRange;
+    components: SpellComponents;
+    duration: SpellDuration[];
+    entries:  string &  SpellEntries[];
+    entriesHigherLevel: SpellEntries[];
+    damageInflict: string[];
+    savingThrow: string[];
+    miscTags: string[];
+    areaTags: string[];
+    classes: SpellClassesGroup;
 }
 
 function TestSpell(props: Props) {
+
+    //Clamp the index from 0 to the max (length)
     let number = props.index;
     if (number >= props.max){
         number = props.max-1
@@ -21,27 +116,35 @@ function TestSpell(props: Props) {
         number = 0
     }
 
+    console.log("Clamp completed")
+
+    let currentSpell : Spell = JSON.parse(JSON.stringify(data.spell[number]))
+
     /* Spell level */
-    let school = stringers.schoolStringify(data.spell[number].school)
-    let spell = "Lv." + data.spell[number].level + " " + school.toLowerCase() + " spell."
-    if (data.spell[number].level == 0){
+    let school = stringers.schoolStringify(currentSpell.school)
+    let spell = "Lv." + currentSpell.level + " " + school.toLowerCase() + " spell."
+    if (currentSpell.level == 0){
         spell = school + " cantrip."
     }
 
+    console.log("Spell type completed")
+
     /* Casting time */
     let time = "";
-    for (let i = 0; i < data.spell[number].time.length; i++) {
+    for (let i = 0; i < currentSpell.time.length; i++) {
         if (i == 0){
-            time = data.spell[number].time[i].number.toString() + " " + data.spell[number].time[i].unit.toString() + " "
+            time = currentSpell.time[i].number.toString() + " " + currentSpell.time[i].unit.toString() + " "
         } else {
-            time += "or " + data.spell[number].time[i].number.toString() + " " + data.spell[number].time[i].unit.toString() + " "
+            time += "or " + currentSpell.time[i].number.toString() + " " + currentSpell.time[i].unit.toString() + " "
         }
     }
+
+    console.log("Spell time completed")
 
     /* Components */
     let components = ""
     let first = 0
-    if (data.spell[number].components.v){
+    if (currentSpell.components.v){
         if (first > 0) {
             components += ", "
         }
@@ -49,7 +152,7 @@ function TestSpell(props: Props) {
         first += 1;
     }
         
-    if (data.spell[number].components.s){
+    if (currentSpell.components.s){
         if (first > 0) {
             components += ", "
         }
@@ -57,41 +160,79 @@ function TestSpell(props: Props) {
         first += 1;
     }
 
-    if (data.spell[number].components.m != null){
+    if (currentSpell.components.m != null){
         if (first > 0) {
             components += ", "
         }
         components += "M"
-        components += " (" + data.spell[number].components.m +")"
+        if (typeof(currentSpell.components.m) == 'string')
+            components += " (" + currentSpell.components.m +")"
+        else {
+            components += " (" + currentSpell.components.m.text + ")"
+            if (currentSpell.components.m.consume)
+                components += " [Consume]"
+        }
         first += 1;
     }
 
+    console.log("Spell components completed")
+
     /* Duration */
     let duration = ""
-    /*
-    for (let i = 0; i < data.spell[number].duration.length; i++) {
+    for (let i = 0; i < currentSpell.duration.length; i++) {
         if (i == 0){
-            console.log(data.spell[number].duration[i])
-            if (data.spell[number].duration[i].type = "instant"){
+            console.log(currentSpell.duration[i])
+            if (currentSpell.duration[i].type = "instant"){
                 duration += "Instantaneous"
-            } else if (data.spell[number].duration[i].type = "timed") {
-                console.log(data.spell[number].duration[i])
-                //data.spell[number].duration[i].
+            } else if (currentSpell.duration[i].type = "timed") {
+                console.log(currentSpell.duration[i])
+                for (let index = 0; index < currentSpell.duration.length; index++) {
+                    if (index != 0) {
+                        duration += " or " + currentSpell.duration[index].duration.amount
+                        if (currentSpell.duration[index].duration.amount > 1)
+                            duration += " " + currentSpell.duration[index].duration.type + "s"
+                        else
+                            duration += " " + currentSpell.duration[index].duration.type
+
+                        if (currentSpell.duration[index].concentration)
+                            duration += " [C]"
+                    } else {
+                        duration += currentSpell.duration[index].duration.amount
+                        if (currentSpell.duration[index].duration.amount > 1)
+                            duration += " " + currentSpell.duration[index].duration.type + "s"
+                        else
+                            duration += " " + currentSpell.duration[index].duration.type
+
+                        if (currentSpell.duration[index].concentration)
+                            duration += " [C]"
+                    }
+                }
             }
         } else {
 
         }
     }
-    */
+
+    console.log("Spell duration completed")
 
     let description = ""
     let higherLevel = ""
-    for (let index = 0; index < data.spell[number].entries.length; index++) {
-        description += data.spell[number].entries[index] + "\n"
+
+    if (currentSpell.entries != null)
+    for (let index = 0; index < currentSpell.entries.length; index++) {
+        if (index > 0 && currentSpell.entries[index].entries != null) {
+            description += "\n - " + currentSpell.entries[index].name + ": "+ currentSpell.entries[index].entries
+        } else {
+            description += currentSpell.entries[index]
+        }
     }
-    for (let index = 0; index < data.spell[number].entriesHigherLevel![0].entries.length; index++) {
-        higherLevel += data.spell[number].entriesHigherLevel![0].entries[index]
+
+    if (currentSpell.entriesHigherLevel != null)
+    for (let index = 0; index < currentSpell.entriesHigherLevel![0].entries.length; index++) {
+        higherLevel += currentSpell.entriesHigherLevel![0].entries[index]
     }
+
+    console.log("Search for damages")
     //Search for any type of damage   eg:  {@damage 1d6} or {@damage 2d8}
 
     function retreiveDamageFromString(string : string, startIntex : number) {
@@ -145,12 +286,12 @@ function TestSpell(props: Props) {
         higherLevel = higherLevel.replaceAll("{"+damagesRAWHigherLevel[index]+"}", damagesHigherLevel[index])
     }
     
-    
+    console.log("Spell description completed")
 
 
     return (
         <table id="test-spell">
-            <th id="name">{data.spell[number].name}</th>
+            <th id="name">{currentSpell.name}</th>
             <tr>{spell}</tr>
             <tr>Casting time: {time}</tr>
             <tr>Components: {components}</tr>
@@ -163,17 +304,6 @@ function TestSpell(props: Props) {
 }
 
 /*
-            
-
-            <tr id="source">Casting Time: {data.spell[number].time[0]}</tr>
-
-            <p id="source">Range {data.spell[number].range}</p>
-
-            <p id="source">Components: {data.spell[number].components}</p>
-
-            <p id="source">Duration: {data.spell[number].duration[0]}</p>
-
-            <p style={{marginTop: "10px"}}>{data.spell[number].entries[0]}</p>
             */
 
 export default TestSpell
