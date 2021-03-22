f7a6248f21489a04b3eea3956c29f11cee6d07c8
LunaeSomnia <adrianmoralestorrano@gmail.com>
2021-03-22 17:47:21 +0100

    Revert "Spell with new look (css)"
    This reverts commit 32daf515ea84e88cec39dc989b14e155efe86c51.

--- src/other/stringers.tsx
+++ src/other/stringers.tsx
@@ -24,42 +24,9 @@ export function schoolStringify(school: string) {
     case 'N':
       return "Necromancy"
       break;
-
+  
     default:
       return "Owo"
-      break;
-  }
-}
-
-export function schoolStringifyColorsPure(school: string) {
-  switch (school) {
-    case 'A':
-      return "#ffffff"
-      break;
-    case 'C':
-      return "#4559c9"
-      break;
-    case 'D':
-      return "#a546ca"
-      break;
-    case 'E':
-      return "#54c945"
-      break;
-    case 'V':
-      return "#c94545"
-      break;
-    case 'I':
-      return "#c98045"
-      break;
-    case 'T':
-      return "#e0dd30"
-      break;
-    case 'N':
-      return "#000000"
-      break;
-
-    default:
-      return "#ffffff"
       break;
   }
 }
--- src/react/TestSpell.tsx
+++ src/react/TestSpell.tsx
@@ -3,27 +3,132 @@ import { render } from 'react-dom'
 import * as data from '../../data/spells/spells-xge.json'
 import * as stringers from '../other/stringers'
 
+interface Serializable<T> {
+    deserialize(input: Object): T;
+}
+
+interface Props {
+    index: number;
+    max: number;
+}
+
+interface SpellSource {
+    source: string,
+    page: number
+}
+
+interface SpellTime{
+    number: number,
+    unit: string,
+    condition: string
+}
+
+interface SpellRange {
+    type: string,
+    distance : SpellRangeDistance
+}
+
+interface SpellRangeDistance {
+    type: string,
+    amount: number
+}
+
+interface SpellComponents {
+    v: boolean,
+    s: boolean,
+    m: SpellComponentsMaterials | string
+}
+
+interface SpellComponentsMaterials {
+    text: string,
+    cost: number,
+    consume: boolean
+}
+
+interface SpellDuration {
+    type: string,
+    duration: SpellDurationDuration
+    ends: string[],
+    concentration: boolean
+}
+
+interface SpellDurationDuration {
+    type: string,
+    amount: number
+}
+
+interface SpellEntries {
+    type: string,
+    name: string,
+    entries: string[]
+}
+
+interface SpellScalingLevelDice {  //Only for cantrips
+    lavel: string,
+    scaling: SpellScalingLevelDiceScaling
+}
+
+interface SpellScalingLevelDiceScaling {
+    1: string,
+    5: string,
+    11: string,
+    17: string
+}
+
+interface SpellClassesGroup {
+    fromClassList: SpellClasses[],
+    fromClassListVariant: SpellClasses[],
+    backgrounds: SpellClasses[]
+}
+
+interface SpellClasses {
+    name: string,
+    source: string,
+    definedInSource: string
+}
+
+interface Spell {
+    name: string;
+    source: SpellSource;
+    otherSources: SpellSource[];
+    level: number;
+    school: string;
+    time: SpellTime[];
+    range: SpellRange;
+    components: SpellComponents;
+    duration: SpellDuration[];
+    entries:  string &  SpellEntries[];
+    entriesHigherLevel: SpellEntries[];
+    damageInflict: string[];
+    savingThrow: string[];
+    miscTags: string[];
+    areaTags: string[];
+    classes: SpellClassesGroup;
+}
 
 function TestSpell(props: Props) {
 
     //Clamp the index from 0 to the max (length)
     let number = props.index;
-    if (number >= props.max) {
-        number = props.max - 1
+    if (number >= props.max){
+        number = props.max-1
     } else if (number < 0) {
         number = 0
     }
 
-    let currentSpell: Spell = JSON.parse(JSON.stringify(data.spell[number]))
+    let currentSpell : Spell = JSON.parse(JSON.stringify(data.spell[number]))
 
     /* Spell level */
     let school = stringers.schoolStringify(currentSpell.school)
-    let level = currentSpell.level;
+    let spell = "Lv." + currentSpell.level + " " + school.toLowerCase() + " spell."
+    if (currentSpell.level == 0){
+        spell = school + " cantrip."
+    }
 
     /* Casting time */
     let time = "";
     for (let i = 0; i < currentSpell.time.length; i++) {
-        if (i == 0) {
+        if (i == 0){
             time = currentSpell.time[i].number.toString() + " " + currentSpell.time[i].unit.toString() + " "
         } else {
             time += "or " + currentSpell.time[i].number.toString() + " " + currentSpell.time[i].unit.toString() + " "
@@ -34,15 +139,15 @@ function TestSpell(props: Props) {
     /* Components */
     let components = ""
     let first = 0
-    if (currentSpell.components.v) {
+    if (currentSpell.components.v){
         if (first > 0) {
             components += ", "
         }
         components += "S"
         first += 1;
     }
-
-    if (currentSpell.components.s) {
+        
+    if (currentSpell.components.s){
         if (first > 0) {
             components += ", "
         }
@@ -50,13 +155,13 @@ function TestSpell(props: Props) {
         first += 1;
     }
 
-    if (currentSpell.components.m != null) {
+    if (currentSpell.components.m != null){
         if (first > 0) {
             components += ", "
         }
         components += "M"
-        if (typeof (currentSpell.components.m) == 'string')
-            components += " (" + currentSpell.components.m + ")"
+        if (typeof(currentSpell.components.m) == 'string')
+            components += " (" + currentSpell.components.m +")"
         else {
             components += " (" + currentSpell.components.m.text + ")"
             if (currentSpell.components.m.consume)
@@ -69,8 +174,8 @@ function TestSpell(props: Props) {
     /* Duration */
     let duration = ""
     for (let i = 0; i < currentSpell.duration.length; i++) {
-        if (i == 0) {
-            if (currentSpell.duration[i].type = "instant") {
+        if (i == 0){
+            if (currentSpell.duration[i].type = "instant"){
                 duration += "Instantaneous"
             } else if (currentSpell.duration[i].type = "timed") {
                 for (let index = 0; index < currentSpell.duration.length; index++) {
@@ -105,30 +210,30 @@ function TestSpell(props: Props) {
     let higherLevel = ""
 
     if (currentSpell.entries != null)
-        for (let index = 0; index < currentSpell.entries.length; index++) {
-            if (index > 0 && currentSpell.entries[index].entries != null) {
-                description += "\n - " + currentSpell.entries[index].name + ": " + currentSpell.entries[index].entries
-            } else {
-                description += currentSpell.entries[index]
-            }
+    for (let index = 0; index < currentSpell.entries.length; index++) {
+        if (index > 0 && currentSpell.entries[index].entries != null) {
+            description += "\n - " + currentSpell.entries[index].name + ": "+ currentSpell.entries[index].entries
+        } else {
+            description += currentSpell.entries[index]
         }
+    }
 
     if (currentSpell.entriesHigherLevel != null)
-        for (let index = 0; index < currentSpell.entriesHigherLevel![0].entries.length; index++) {
-            higherLevel += currentSpell.entriesHigherLevel![0].entries[index]
-        }
+    for (let index = 0; index < currentSpell.entriesHigherLevel![0].entries.length; index++) {
+        higherLevel += currentSpell.entriesHigherLevel![0].entries[index]
+    }
 
     //Search for any type of damage   eg:  {@damage 1d6} or {@damage 2d8}
 
-    function retreiveDamageFromString(string: string, startIntex: number) {
-        let returned: string[] = []
+    function retreiveDamageFromString(string : string, startIntex : number) {
+        let returned : string[] = []
         let exit = false
-
+        
         let index = 0
         let buildingString = ""
         let building = false;
         while (!exit) {
-            if (string.charAt(index) == '{') {
+            if (string.charAt(index) == '{'){
                 building = true
             }
             else if (string.charAt(index) == '}') {
@@ -138,16 +243,17 @@ function TestSpell(props: Props) {
             } else if (building) {
                 buildingString += string.charAt(index)
             }
+            
             if (index < string.length)
-                index += 1
+            index += 1
             else break
         }
-
+        
         return returned;
     }
-
-    let damagesRAW: string[] = retreiveDamageFromString(description, 0)
-    let damages: string[] = []
+    
+    let damagesRAW : string[] = retreiveDamageFromString(description, 0)
+    let damages : string[] = []
 
     //Normalize the damage output  ->  @damage 1d6  :   1d6
     for (let index = 0; index < damagesRAW.length; index++) {
@@ -155,11 +261,11 @@ function TestSpell(props: Props) {
     }
 
     for (let index = 0; index < damagesRAW.length; index++) {
-        description = description.replaceAll("{" + damagesRAW[index] + "}", damages[index])
+        description = description.replaceAll("{"+damagesRAW[index]+"}", damages[index])
     }
 
-    let damagesRAWHigherLevel: string[] = retreiveDamageFromString(higherLevel, 0)
-    let damagesHigherLevel: string[] = []
+    let damagesRAWHigherLevel : string[] = retreiveDamageFromString(higherLevel, 0)
+    let damagesHigherLevel : string[] = []
 
     //Normalize the damage output  ->  @damage 1d6  :   1d6
     for (let index = 0; index < damagesRAWHigherLevel.length; index++) {
@@ -167,55 +273,19 @@ function TestSpell(props: Props) {
     }
 
     for (let index = 0; index < damagesRAWHigherLevel.length; index++) {
-        higherLevel = higherLevel.replaceAll("{" + damagesRAWHigherLevel[index] + "}", damagesHigherLevel[index])
+        higherLevel = higherLevel.replaceAll("{"+damagesRAWHigherLevel[index]+"}", damagesHigherLevel[index])
     }
-
-    let textColor = "#181818"
-    if (school == "Necromancy")
-        textColor = "#fff"
-
-    let spellColor = stringers.schoolStringifyColorsPure(currentSpell.school)
-
-    let borderColor = spellColor;
-    if (school == "Necromancy")
-        borderColor = "#fff"
+    
 
     return (
         <table id="test-spell">
-            <th id="spell-name-div" style={{
-                borderColor: spellColor,
-                color: textColor,
-            }}>
-                <div id="spell-level" style={{
-                    backgroundColor: spellColor,
-                    color: textColor
-                }}>
-                    {level}
-                </div>
-                <div id="spell-name" style={{ color: borderColor }}>
-                    {currentSpell.name}
-                </div>
-                <div id="spell-school" style={{ color: borderColor }}>
-                    {school}
-                </div>
-            </th>
-            <tr id="spell-info-container">
-                <div id="spell-casting-time">
-                    {time}
-                </div>
-                <div id="spell-components">
-                    {components}
-                </div>
-                <div id="spell-duration">
-                    {duration}
-                </div>
-            </tr>
-            <tr id="spell-description" style={{ borderColor: borderColor }}>
-                {description}
-            </tr>
-            <tr id="spell-higher-level" style={{ borderColor: borderColor }} >
-                {higherLevel}
-            </tr>
+            <th id="name">{currentSpell.name}</th>
+            <tr>{spell}</tr>
+            <tr>Casting time: {time}</tr>
+            <tr>Components: {components}</tr>
+            <tr>Duration: {duration}</tr>
+            <tr id="desc">{description}</tr>
+            <tr id="desc" >{higherLevel}</tr>
 
         </table>
     );
